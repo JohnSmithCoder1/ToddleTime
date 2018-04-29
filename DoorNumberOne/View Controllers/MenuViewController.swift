bc19d7d0db030cf24f6ffdcedef5c5c160a99dad
Banana Viking <bananavikingcoder@gmail.com>
2018-04-28 20:46:00 -0500

    Make button changes through IB extension instead of code.

--- DoorNumberOne/View Controllers/MenuViewController.swift
+++ DoorNumberOne/View Controllers/MenuViewController.swift
@@ -14,32 +14,11 @@ class MenuViewController: UIViewController {
     @IBOutlet weak var marketButton: UIButton!
     @IBOutlet weak var numbersButton: UIButton!
     
-    override func viewDidLoad() {
-        farmButton.layer.cornerRadius = 4.5
-        farmButton.layer.shadowColor = UIColor.black.cgColor
-        farmButton.layer.shadowOffset = CGSize(width: 4.5, height: 4.5)
-        farmButton.layer.shadowRadius = 4.5
-        farmButton.layer.shadowOpacity = 1.0
-        
-        marketButton.layer.cornerRadius = 4.5
-        marketButton.layer.shadowColor = UIColor.black.cgColor
-        marketButton.layer.shadowOffset = CGSize(width: 4.5, height: 4.5)
-        marketButton.layer.shadowRadius = 4.5
-        marketButton.layer.shadowOpacity = 1.0
-        
-        numbersButton.layer.cornerRadius = 4.5
-        numbersButton.layer.shadowColor = UIColor.black.cgColor
-        numbersButton.layer.shadowOffset = CGSize(width: 4.5, height: 4.5)
-        numbersButton.layer.shadowRadius = 4.5
-        numbersButton.layer.shadowOpacity = 1.0
-
-        super.viewDidLoad()
-    }
 }
 
+// adds the following settings in IB for all views under attributes inspector
 extension UIView {
     
-    // adds the following settings in IB for all views under attributes inspector
     @IBInspectable
     var cornerRadius: CGFloat {
         get {
