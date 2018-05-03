92078f6a7d4fb8afbdec3442222c250737686855
Banana Viking <bananavikingcoder@gmail.com>
2018-05-03 10:45:34 -0500

    commit minor change before merging branch into master

--- ToddlerTime/View Controllers/ShapesViewController.swift
+++ ToddlerTime/View Controllers/ShapesViewController.swift
@@ -45,6 +45,7 @@ class ShapesViewController: UIViewController {
         } else {
             button.setTitle(emoji, for: .normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            
             if emoji == "🔵" {
                 playSound(forObject: "circle")
             }
