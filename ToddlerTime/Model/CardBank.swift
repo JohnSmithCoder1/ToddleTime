6aa48650c5de6308c4576b3b994f11410f11c23e
Banana Viking <bananavikingcoder@gmail.com>
2018-08-02 10:57:24 -0500

    refactored playSound()

--- ToddlerTime/Model/CardBank.swift
+++ ToddlerTime/Model/CardBank.swift
@@ -43,19 +43,12 @@ class CardBank {
     }
     
     func playSound(forObject: String) {
-        guard let url = Bundle.main.url(forResource: forObject, withExtension: "wav") else {
-            print("url not found")
-            return
-        }
-        
+        guard let url = Bundle.main.url(forResource: forObject, withExtension: "wav") else { return }
         do {
-            // prepares app to takeover the device audio
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player?.play()
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            player.prepareToPlay()
+            player.play()
         } catch let error as NSError {
             print("error: \(error.localizedDescription)")
         }
